49530fce522a9809f346546d13d23f6cb4b5593e
kimc18 <64927627+kimc18@users.noreply.github.com>
2021-05-08 21:43:40 +0200

    srtuffs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Collections.Generic;
 
@@ -14,23 +14,41 @@ namespace HangMan
             Thread.Sleep(1000); 
             Console.Clear();
 
+            Instructions();
+            Thread.Sleep(3000); 
+            Console.Clear();
+
             PlayGame();
-            EndGame();
+            //EndGame();
         }
 
         //splash screen
         private static void StartGame()
         {
-            Console.WriteLine(" ---------------- ");
-            Console.WriteLine("     HANGMAN!     ");
-            Console.WriteLine(" ---------------- ");
+            Console.WriteLine("  _    _          _   _  _____ __  __          _   _  ");
+            Console.WriteLine(" | |  | |   /\\   | \\ | |/ ____|  \\/  |   /\\   | \\ | | ");
+            Console.WriteLine(" | |__| |  /  \\  |  \\| | |  __| \\  / |  /  \\  |  \\| | ");
+            Console.WriteLine(" |  __  | / /\\ \\ | . ` | | |_ | |\\/| | / /\\ \\ | . ` | ");
+            Console.WriteLine(" | |  | |/ ____ \\| |\\  | |__| | |  | |/ ____ \\| |\\  | ");
+            Console.WriteLine(" |_|  |_/_/    \\_\\_| \\_|\\_____|_|  |_/_/    \\_\\_| \\_| ");
+            
+        }
+
+        static void Instructions()
+        {
+            System.Console.WriteLine("1. Type 1 character then press enter.");
+            System.Console.WriteLine("2. You have 5 lives, when you have no lives left the game ends.");
+            System.Console.WriteLine("3. Goodluck!");
+            System.Console.WriteLine("(This set of instructions will disappear in 3 seconds)");
         }
 
         private static void PlayGame()
         {
-            //int[] positions = new int[] {}; //this is giving an exception cos maybe the size isnt defined well
-            var positions = new List<int>();
-            int lives = 3;
+            var life = new Health
+            {
+                Lives = 5
+            };
+            
             string underscores = "";
             
             //for appending underscores
@@ -38,14 +56,15 @@ namespace HangMan
 
             //instantiate random number generator
             Random rnd1 = new Random();
-            int randomIndexOfArray = rnd1.Next(0, 3);
-            //Console.WriteLine(randomIndexOfArray);
+            int randomIndexOfArray = rnd1.Next(0, 5);
 
             //words used for hangman
-            String[] wordsThatWillBeUsed = new String[3];
+            String[] wordsThatWillBeUsed = new String[5];
             wordsThatWillBeUsed[0] = "poopies";
             wordsThatWillBeUsed[1] = "kim";
             wordsThatWillBeUsed[2] = "calculator";
+            wordsThatWillBeUsed[3] = "jubby";
+            wordsThatWillBeUsed[4] = "peepidy";
 
             //generate underlines to show length of word, underscores stored in sb
             for (int i = 0; i < wordsThatWillBeUsed[randomIndexOfArray].Length; i++)
@@ -56,7 +75,12 @@ namespace HangMan
             //all the underscores got added into a string
             string new_underscores = sb.ToString();
             char[] charArrUnder = new_underscores.ToCharArray(); //spaces are their own element, each _ is at char[i*2]
-            Console.WriteLine(charArrUnder.Length);
+            List<char> wrongLetters = new List<char>();
+
+            //Console.WriteLine(charArrUnder.Length);
+            System.Console.Write(charArrUnder);
+            System.Console.WriteLine("");
+            System.Console.Write(life.Lives);
 
             //ask user for input then place the character in correct position and keep underscores
             do
@@ -64,6 +88,7 @@ namespace HangMan
                 string userInput = Console.ReadLine();
                 char validInput= CheckUserInput(userInput);
                 string validInputString = validInput.ToString();
+                string charArrUnder_String = new_underscores;
                 
                 //this will refresh the screen so that you dont get unnecessary text, it will also place the valid input in place 
                 if (wordsThatWillBeUsed[randomIndexOfArray].Contains(validInput))
@@ -76,10 +101,30 @@ namespace HangMan
                             charArrUnder[i*2] = validInput;
                         }
                     }
-                    string charArrUnder_String = new string(charArrUnder);
+                    charArrUnder_String = new string(charArrUnder);
+                    //charArrUnder_String = charArrUnder.ToString();
                     Console.WriteLine(charArrUnder_String);
                     Console.Clear();
                     Console.WriteLine(charArrUnder_String);
+                    System.Console.Write("Lives left: ");
+                    System.Console.WriteLine(life.Lives);
+                    wrongLetters.ForEach(System.Console.Write);
+                }
+                else
+                {
+                    life.Lives--;
+                    Console.Clear();
+                    wrongLetters.Add(validInput);
+                    Console.WriteLine(charArrUnder_String);
+                    wrongLetters.ForEach(System.Console.Write);
+                    System.Console.Write("\nLives left: ");
+                    System.Console.WriteLine(life.Lives);
+                }
+
+                if (life.Lives == 0)
+                {
+                    EndGame();
+                    System.Environment.Exit(0);
                 }
 
             } while (Array.Exists(charArrUnder, element => element == '_'));
@@ -89,6 +134,7 @@ namespace HangMan
         private static void EndGame()
         {
             Console.WriteLine("\n\n End!");
+            Thread.Sleep(3000); 
         }
 
         private static char CheckUserInput(string a) //takes string as input, if length is of 1 char then convert to char and return char
